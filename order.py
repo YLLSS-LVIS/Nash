cf8b401ff30fb7892290f437c545e1c774ad1aa8
YLLSS-finance <yllss.lvis@gmail.com>
2026-03-09 15:57:32 +0800

    Added order class
     nothing is going on, nothing to see here

--- order.py
+++ order.py
@@ -1,8 +1,38 @@
 class order:
-    def __init__(self, mpid, contractID, price, side, qty):
+    __slots__ = [
+        "mpid",
+        "contractID",
+        "orderID",
+        "price",
+        "side",
+        "qty",
+        "head",
+        "tail",
+    ]
+
+    def __init__(
+        self, mpid, orderID, contractID, price, side, qty, head=None, tail=None
+    ):
         self.mpid = mpid
         self.contractID = contractID
+        self.orderID = orderID
         self.price = price
         self.side = side
         self.qty = qty
-        self.head, self.tail = None, None
+        self.head, self.tail = head, tail
+
+    def serialise_link(self, link):
+        if link is None:
+            return None
+        return [link.mpid, link.orderID]
+
+    def serialise(self):
+        return [
+            self.mpid,
+            self.orderID,
+            self.price,
+            self.side,
+            self.qty,
+            self.serialise_link(self.head),
+            self.serialise_link(self.tail),
+        ]
